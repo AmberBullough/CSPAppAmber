4abf20340892029947d4354fc75d7f495b2dad1e
Amber Bullough <amber@bertbullough.com>
2017-11-08 09:54:49 -0700

    Took out code

--- CSP App Amber/Controller/ViewController.swift
+++ CSP App Amber/Controller/ViewController.swift
@@ -11,12 +11,7 @@ import UIKit
 class ViewController: UIViewController
 {
 
-    @IBOutlet weak var AlgorithmsSegue: UIButton!
-    
-    @IBAction func AlgorithmsSend(_ sender: UIButton)
-    {
-        
-    }
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
