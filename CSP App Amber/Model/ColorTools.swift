5b58c526433955fdca024d5bfebaf1594c7a173a
Amber Bullough <amber@bertbullough.com>
2017-11-20 09:42:55 -0700

    added color tools

--- CSP App Amber/Model/ColorTools.swift
+++ CSP App Amber/Model/ColorTools.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2017 CTEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class ColorTools
+{
+    public func createColor() -> UIColor
+    {
+        let newColor :UIColor
+        let redAmount = CGFloat(Double(arc4random_uniform(256))/255.00)
+        let greenAmount = CGFloat(Double(arc4random_uniform(256))/255.00)
+        let blueAmount = CGFloat(Double(arc4random_uniform(256))/255.00)
+        newColor = UIColor( red: redAmount, green: greenAmount, blue: blueAmount, alpha: CGFloat(1.0))
+        
+        return newColor
+    }
+}
