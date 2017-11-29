10af7ef48810adda4411f779143519a504620c88
Amber Bullough <amber@bertbullough.com>
2017-11-29 08:17:18 -0700

    Added into to detail

---
+++ CSP App Amber/Controller/InternetDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  InternetDetailViewController.swift
+//  CSP App Amber
+//
+//  Created by Bullough, Amber on 11/29/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+public class InternetDetailViewController : UIViewController
+{
+    //Mark:GUIControlls
+    @IBOutlet weak var textView: UILabel!
+    @IBOutlet weak var webViewer: WKWebView!
+    
+    override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() -> Void
+    {
+        
+    }
+    }
