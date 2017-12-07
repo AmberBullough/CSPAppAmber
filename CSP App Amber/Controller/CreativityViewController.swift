43996d0ed5eed66eaaf292ece27b47bcee9faaa4
Amber Bullough <amber@bertbullough.com>
2017-12-07 08:03:36 -0700

    Added photos and pictures to app. Added code to creativity view controller

--- CSP App Amber/Controller/CreativityViewController.swift
+++ CSP App Amber/Controller/CreativityViewController.swift
@@ -8,3 +8,54 @@
 
 import UIKit
 
+public class CreativityViewController : UICollectionViewController, UICollectionViewDelegateFlowLayout
+{
+    private let reuseIdentifier = "artIdentifier"
+    private let sectionInserts = UIEdgeInsets(top:  50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let itemsPerRow : CGFloat = 3
+    
+    private lazy var artSelection : [UIImage?] =
+    {
+        return [
+            UIImage(named: "cute"),
+            UIImage(named: "Arty"),
+            UIImage(named: "Ia"),
+            UIImage(named: "Melanie"),
+            UIImage(named: "Sam"),
+            UIImage(named: "Ollie"),
+            UIImage(named: "1x binary"),
+            UIImage(named: "Badger"),
+            UIImage(named: "tree pic")
+            ]
+    }()
+    
+    var largePhotoIndexPath: IndexPath?
+    {
+       didSet
+       {
+        var indexPaths = [IndexPath]()
+        if let largePhotoIndexPath = largePhotoIndexPath
+        {
+            indexPaths.append(largePhotoIndexPath)
+        }
+        if let oldValue = oldValue
+        {
+            indexPaths.append(oldValue)
+        }
+        
+        collectionView?.performBatchUpdates(
+            {
+                self.collectionView?.reloadItems(at: indexPaths)
+                
+        })
+        {
+            completed in
+            if let largePhotoIndexPath = self.largePhotoIndexPath
+            {
+                self.collectionView?.scrollToItem(at: largePhotoIndexPath,
+                                                  at: .centeredVertically,
+                                                  animated: true)
+            }
+        }
+    }
+}
