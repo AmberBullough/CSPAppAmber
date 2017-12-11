c2145c28193891aa8aaf10f727ac394ebbd3dfe8
Amber Bullough <amber@bertbullough.com>
2017-12-11 08:02:31 -0700

    Added guests

--- CSP App Amber/Controller/AppDelegate.swift
+++ CSP App Amber/Controller/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     var window: UIWindow?
 
+    
 
     internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
