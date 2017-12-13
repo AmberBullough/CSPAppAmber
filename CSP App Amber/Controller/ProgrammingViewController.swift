19d1251d31134900aeb6a8a73777cfdd0caea08d
Amber Bullough <amber@bertbullough.com>
2017-12-13 09:11:55 -0700

    Moved stuff and added code

--- CSP App Amber/Controller/ProgrammingViewController.swift
+++ CSP App Amber/Controller/ProgrammingViewController.swift
@@ -7,3 +7,23 @@
 //
 
 import UIKit
+import SpriteKit
+
+public class ProgrammingViewController : UIViewController
+{
+    //Mark: UI Components
+    override public func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+    let gameScene = StartScene(size: view.bounds.size)
+    let gameView = view as! SKView
+        
+        gameView.showsFPS = true
+        gameView.showsNodeCount = true
+        gameView.ignoresSiblingOrder = true
+        
+        gameScene.scaleMode = .resizeFill
+        gameView.presentScene(gameScene)
+    
+    }
+}
