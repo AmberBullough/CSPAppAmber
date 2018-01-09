3c6ca7b8e92d92362fba4df5d07606bd27598200
Amber Bullough <amber@bertbullough.com>
2018-01-09 07:46:16 -0700

    Added Step 1 of DataViewController Setup

---
+++ CSP App Amber/Controller/DataView/BucketItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  BucketItemCell.swift
+//  CSP App Amber
+//
+//  Created by Bullough, Amber on 1/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell : UITableViewCell
+{
+    
+}
---
+++ CSP App Amber/Controller/DataView/DataViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DataViewController.swift
+//  CSP App Amber
+//
+//  Created by Bullough, Amber on 1/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController : UITableViewController
+{
+    
+}
